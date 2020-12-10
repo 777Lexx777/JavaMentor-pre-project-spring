15f2958009dc5e85988df986dec3e837be276ee6
geekbrains <lex156166@gmail.com>
2020-12-10 20:58:00 +0400

    Lesson_2

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,24 @@
 package app.config;
 
+import app.model.*;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
+    @Bean(name="dog")
+ //   @Scope("prototype")
+    public Dog getDog() {
+        return new Dog();
+    }
+
+    @Bean(name="timer")
+    @Scope("singleton")
+    public Timer getTimer() {
+        return new Timer();
+    }
 }
