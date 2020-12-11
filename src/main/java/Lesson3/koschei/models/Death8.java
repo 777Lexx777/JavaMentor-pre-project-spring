4e264bc86353d1ad3c60c4c13e0ffe1370210296
geekbrains <lex156166@gmail.com>
2020-12-12 03:59:24 +0400

    Lesson 3 - file "Lesson3" - 11 file "pom.xml" - 1;

---
+++ src/main/java/Lesson3/koschei/models/Death8.java
@@ -0,0 +1,10 @@
+package Lesson3.koschei.models;
+
+
+public class Death8 {
+
+    @Override
+    public String toString() {
+        return " Сломай Иглу!!! ";
+    }
+}
---
+++ src/main/java/Lesson3/koschei/models/Needle7.java
@@ -0,0 +1,16 @@
+package Lesson3.koschei.models;
+
+
+public class Needle7 {
+
+    private Death8 death;
+
+    public Needle7(Death8 death) {
+        this.death = death;
+    }
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( " + death.toString() + ")";
+    }
+}
